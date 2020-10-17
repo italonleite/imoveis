394a36e70c138b692c46630a0dfb13f75635682a
italo.leite <italo.leite@nibo.com.br>
2020-10-17 15:47:11 -0300

    criando teste unitário com a classe anuncio

---
+++ src/Imoveis.Test/AnuncioTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace Imoveis.Test
+{
+    public class AnuncioTest
+    {
+        [Fact]
+        public void UsuarioDeveCriarAnuncio()
+        {
+            //arrange
+            //action
+            //assert
+        }
+    }
+}
