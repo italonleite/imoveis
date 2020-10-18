8ea9f5b432e08951ee8c383425dae58900bdf18a
italo.leite <italo.leite@nibo.com.br>
2020-10-18 09:02:42 -0300

    criando classe entity

---
+++ src/Imoveis.Business/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace Imoveis.Business.Models
+{
+    public abstract class Entity
+    {
+       
+        public ValidationResult ValidationResult { get; protected set; }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
