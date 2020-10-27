dee1fb060f3f91332458dd3f2e98723fce5dc261
italo.leite <italo.leite@nibo.com.br>
2020-10-27 08:18:17 -0300

    imovel tem um endereco

---
+++ src/Imoveis.Business/Models/Imovel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imoveis.Business.Models
+{
+    public class Imovel
+    {
+        public Guid ImovelId { get; set; }
+        public string Nome { get; set; }
+        public ECaracteristica ECaracteristica { get; set; }
+        public Guid EnderecoId { get; set; }
+        public Endereco Endereco { get; set; }
+
+        public Imovel()
+        {
+
+        }
+
+        public Imovel(Guid imovelId, string nome, ECaracteristica eCaracteristica, Guid enderecoId, Endereco endereco)
+        {
+            ImovelId = imovelId;
+            Nome = nome;
+            ECaracteristica = eCaracteristica;
+            EnderecoId = enderecoId;
+            Endereco = endereco;
+        }
+    }
+
+}
